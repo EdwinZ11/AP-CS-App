33112d5887e8ff5371c73a124ea3d638a7251be8
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2022-09-15 12:30:10 -0400

    9/15/22

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,7 +1,29 @@
 import "./style.css";
 
 const DOMSelectors = {
-  startBtn: document.getElementsByClassName("startBtn"),
+  button: document.querySelector(".startBtn"),
+  title: document.querySelector(".title"),
+  mainDisplay: document.querySelector(".mainBody"),
+  getCard: document.querySelector(".getCard"),
 };
 
-DOMSelectors.startBtn.addEventListener("click", console.log("hi"));
+function getGame() {
+  DOMSelectors.button.addEventListener("click", function () {
+    DOMSelectors.mainDisplay.innerHTML = "";
+    DOMSelectors.mainDisplay.insertAdjacentHTML(
+      "afterend",
+      `<button class="getCard">Get Card!</button>
+    <div class="dealHand">
+    <h2 class="text">Dealer's Hand</h2>
+    </div>
+    <div class="userHand">
+    <h2 class="text">Your Hand</h2>
+    </div>`
+    );
+  });
+
+  const getCard = document.querySelector(".getCard");
+  getCard.addEventListener("click", function () {
+    console.log("yes");
+  });
+}
