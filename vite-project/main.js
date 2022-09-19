5b711dac806dc1307927593249f153f344377635
EdwinZ11 <90789780+EdwinZ11@users.noreply.github.com>
2022-09-19 12:30:05 -0400

    9/19/22

--- vite-project/main.js
+++ vite-project/main.js
@@ -1,31 +1,59 @@
 import "./style.css";
 
-var userSum = 0;
+var userSum = 22;
 var dealerSum = 0;
 
-var deck = [];
 var win = false;
 var lose = false;
 
 const DOMSelectors = {
   hit: document.querySelector(".hit"),
   stand: document.querySelector(".stand"),
-  winLose: document.querySelector(".userHand"),
+  userHand: document.querySelector(".userHand"),
 };
 
 function Lose() {
-  DOMSelectors.hit.addEventListener("click", function () {
-    if (userSum > 21) {
-      return (lose = true);
-    } else {
-      console.log("error");
+  function clickLose() {
+    DOMSelectors.hit.addEventListener("click", function () {
+      if (userSum > 21) {
+        DOMSelectors.userHand.insertAdjacentText("afterend", `You Lose!`);
+      } else {
+        console.log("error");
+      }
+    });
+  }
+  clickLose();
+}
+
+Lose();
+
+const Deck = [];
+
+var deck1 = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+var deck2 = ["C", "H", "S", "D"];
+
+async function createDeck() {
+  try {
+    for (let i = 0; i < 1; i++) {
+      Deck.push(deck1);
     }
-  });
+  } catch {
+    console.log("errror");
+  }
+}
+createDeck();
+
+console.log(Deck);
+/* function userLose() {
   if ((lose = true)) {
-    DOMSelectors.winLose.insertAdjacentText("afterbegin", `You Lose!`);
+    DOMSelectors.winLose.insertAdjacentText("afterend", `You Lose!`);
   } else {
     console.log("error");
   }
 }
-
-DOMSelectors.stand.addEventListener("click", console.log(lose));
+function userWin() {
+  if ((win = ture)) {
+    DOMSelectors.winLose.insertAdjacentText("afterend", `You Win!`);
+  }
+}
+*/
